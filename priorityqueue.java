e4a8ef65cdfd36471e4aebdebfcdf3f2ba99606f
ishaan <ishaankedar2004@gmail.com>
2025-01-30 21:05:19 +0530

    priority queue

---
+++ priorityqueue.java
@@ -0,0 +1,21 @@
+// Java Program for PriorityQueue
+import java.util.PriorityQueue;
+
+public class priorityqueue 
+{
+    public static void main(String[] args) 
+    {
+          // Priority Queue Min Type
+        PriorityQueue<Integer> p = new PriorityQueue<>();
+
+        // Add elements to the queue
+        p.add(3);
+        p.add(10);
+        p.add(7);
+        p.add(2);
+
+        // Print the head of the queue
+        System.out.println("Head of Queue : " + p.peek());
+
+    }
+}
